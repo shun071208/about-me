ce30f00c92b20c552fc85c93b732e5caf3079828
shun071208 <274154284+shun071208@users.noreply.github.com>
2026-08-01 11:03:37 +0900

    kimatu

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,66 @@
-window.addEventListener('DOMContentLoaded', () => {
-    alert('自己紹介サイトへようこそ！');
-    console.log('自己紹介サイトへようこそ！');
+const cards = document.querySelectorAll(".fadein");
+
+window.addEventListener("scroll", () => {
+
+    cards.forEach(card => {
+
+        const position = card.getBoundingClientRect().top;
+
+        if(position < window.innerHeight - 100){
+            card.classList.add("show");
+        }
+
+    });
+
+});
+
+const form = document.querySelector("#contactForm");
+
+if(form){
+
+form.addEventListener("submit",(e)=>{
+
+const name = document.querySelector("#name").value;
+
+if(name === ""){
+    alert("名前を入力してください");
+    e.preventDefault();
+}
+
+});
+
+}
+// ページ表示時のフェードイン
+window.addEventListener("load", () => {
+    document.body.classList.add("page-show");
+});
+
+
+// ページ移動時のフェードアウト
+const links = document.querySelectorAll("a");
+
+links.forEach(link => {
+
+    link.addEventListener("click", (e) => {
+
+        const url = link.href;
+
+        // 外部リンクやページ内リンクは除外
+        if(
+            url.includes("#") ||
+            link.target === "_blank"
+        ){
+            return;
+        }
+
+        e.preventDefault();
+
+        document.body.classList.remove("page-show");
+
+        setTimeout(() => {
+            location.href = url;
+        },500);
+
+    });
+
 });
